499d2e695a716f2803dd743d4b0e6fdebc053dee
Dmitry Chirkin <josser@nixsolutions.com>
2022-04-24 23:49:50 +0300

    Removed console.log

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,10 @@ import './providers/index.js'
 import app from './app.js';
 import http from 'http';
 import config from './config.js';
+import { container } from 'tsyringe';
+import type { Logger } from 'pino';
 
+const logger = container.resolve<Logger>('logger');
 http.createServer(app.callback()).listen(config.app.port, () => {
-  console.log(`Server is running on port ${config.app.port}`);
+ logger.info(`Server is running on port ${config.app.port}`);
 });
